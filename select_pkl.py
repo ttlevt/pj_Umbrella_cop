30113b6c911736cc21dcb432259c52491a4b6da3
ttlevt <ttlevt@gmail.com>
2020-03-16 14:23:03 +0900

    'finally'

--- select_pkl.py
+++ select_pkl.py
@@ -1,29 +1,29 @@
+import pandas as pd
 def select_pkl(place_name, data_rst):
-    import pandas as pd
     lc = pd.read_csv('./apis/sector.csv', encoding='CP949')
     import pickle
     if place_name in lc['kk'].values:
-        from sklearn import tree
-        tree = pickle.load(open("weather_kk.pkl", "rb"))
+        # from sklearn import tree
+        tree = pickle.load(opipen("weather_kk.pkl", "rb"))
         result= tree.predict(data_rst)
     elif place_name in lc['kw'].values:
-        from sklearn import tree
+        # from sklearn import tree
         tree = pickle.load(open("weather_kw.pkl", "rb"))
         result= tree.predict(data_rst)
     elif place_name in lc['cc'].values:
-        from sklearn import tree
+        # from sklearn import tree
         tree = pickle.load(open("weather_cc.pkl", "rb"))
         result= tree.predict(data_rst)
     elif place_name in lc['jl'].values:
-        from sklearn import tree
+        # from sklearn import tree
         tree = pickle.load(open("weather_jl.pkl", "rb"))
         result= tree.predict(data_rst)
     elif place_name in lc['ks'].values:
-        from sklearn import tree
+        # from sklearn import tree
         tree = pickle.load(open("weather_ks.pkl", "rb"))
         result = tree.predict(data_rst)
     else:
-        from sklearn import tree
+        # from sklearn import tree
         tree = pickle.load(open("weather_jj.pkl", "rb"))
         result = tree.predict(data_rst)
     return result
