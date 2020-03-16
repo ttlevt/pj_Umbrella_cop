8ebb76757c13e4fecab920c689cf93ceb619df22
ttlevt <ttlevt@gmail.com>
2020-03-17 07:32:15 +0900

    0316evn

--- now_sk.py
+++ now_sk.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
+from select_pkl import select_pkl
 
 def now_sk(place_name):
     #이름값 조건문 적어놓기
@@ -17,14 +17,31 @@ def now_sk(place_name):
     baseurl = 'https://apis.openapi.sk.com/weather/current/hourly?'
     skey = '&appKey=l7xxbca72c23f2a144839a2c531cfdde3ec3'
     url_ = baseurl+xy+skey
-    print(url_)
+    # print(url_)
     response = requests.get(url_)
     assert response.status_code is 200
     dom = BeautifulSoup(response.content, "html.parser")
-    # cd = dom.find()
-    print(dom)
-    # print(dom)
-    # print(dom['SKY'].values())
+
+    zs = str(dom)
+
+
+    mm = zs.split(',')[14][-5:-1]
+    # print('mm:',mm)
+    percent = zs.split(',')[10][-6:-1]
+
+    sky = zs.split(',')[6][-4:-2]
+    skytr = ''
+    if sky == '맑음':
+        skytr = 0
+    else:
+        skytr = 1
+    print(url_)
+    print('percent:', percent)
+    print('sky:', skytr)
+    now_li = [skytr, mm, percent]
+    print(now_li)
+    # result = select_pkl(place_name, apis_today)
+
 
 now_sk('광주시')
 
