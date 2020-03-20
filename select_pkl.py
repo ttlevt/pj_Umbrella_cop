dfd3da013e80101fc8efd3cae4ed2c34e6720412
ttlevt <ttlevt@gmail.com>
2020-03-20 10:53:08 +0900

    mamam

--- select_pkl.py
+++ select_pkl.py
@@ -3,36 +3,39 @@ import pandas as pd
 def select_pkl(place_name, data_rst):
     from sklearn.tree import DecisionTreeClassifier
     lc = pd.read_csv('./apis/sector.csv', encoding='CP949')
+    # 지역분류파일 read
     import pickle
-    print(place_name)
+    # print(place_name)
 # try:
 # print(lc)
     # tree = pickle.load(open("weather_kk.pkl", "rb"))
     # result = tree.predict(data_rst)
     # return result
     if place_name in lc['kk'].values:
+        # 요청명이 지역별 컬럼에 해당하는값이라면
         tree = pickle.load(open("weather_kk.pkl", "rb"))
-        print('kk')
+        # 해당지역의 자료로 피클링했던 알고리즘을 언피클링해서 결과를 반환함
+        # print('kk')
         result= tree.predict(data_rst)
     elif place_name in lc['kw'].values:
         tree = pickle.load(open("weather_kw.pkl", "rb"))
-        print('kw')
+        # print('kw')
         result= tree.predict(data_rst)
     elif place_name in lc['cc'].values:
         tree = pickle.load(open("weather_cc.pkl", "rb"))
-        print('cc')
+        # print('cc')
         result= tree.predict(data_rst)
     elif place_name in lc['jl'].values:
-        print('jl')
+        # print('jl')
         tree = pickle.load(open("weather_jl.pkl", "rb"))
         result= tree.predict(data_rst)
     elif place_name in lc['ks'].values:
-        print('ks')
+        # print('ks')
         tree = pickle.load(open("weather_ks.pkl", "rb"))
         result = tree.predict(data_rst)
     else:
         tree = pickle.load(open("weather_jj.pkl", "rb"))
-        print('jj')
+        # print('jj')
         result = tree.predict(data_rst)
     # except Exception as e:
         # print(e)
